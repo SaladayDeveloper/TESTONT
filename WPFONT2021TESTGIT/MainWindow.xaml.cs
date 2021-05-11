b56c297c0ca602e893f3af6d535a7d06549b379e
Пользователь <@IT-CUBE-NB>
2021-05-11 10:41:33 +0500

    Hello world

--- WPFONT2021TESTGIT/MainWindow.xaml.cs
+++ WPFONT2021TESTGIT/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFONT2021TESTGIT
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
